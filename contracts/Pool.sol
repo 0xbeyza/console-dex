e02c09669b7836f2f8e9a254cd50af3d610225b4
Mehmet Can Kenar <kenarm18@itu.edu.tr>
2025-01-08 16:15:31 +0300

    kontrat 3 fonksiyon tamam

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -4,58 +4,137 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Pool {
-    IERC20 public tokenA;
-    IERC20 public tokenB;
-    uint256 public totalSupply;
-   
-    
-    struct userBalance{
-        uint256 balance0;
-        uint256 balance1;
-    }
+contract Pool 
+{
+
+    // Variables
+
+            IERC20 public tokenA;
+            IERC20 public tokenB;
+            uint256 public totalSupply;
+            uint256 public ratio;
+            uint256 balanceA;
+            uint256 balanceB;
+            
 
     mapping(address => userBalance) public balances;
-    uint256 public ratio;
+
     // Constructor
-    constructor(address _token0, address _token1) {
-        tokenA = IERC20(_token0);
-        tokenB = IERC20(_token1);
-        tokenA.approve(address(this), 100);
-        tokenB.approve(address(this), 100);
-        tokenA.transferFrom(msg.sender, address(this), 100);
-        tokenB.transferFrom(msg.sender, address(this), 100);
-        
-    }
 
-    // 1. Add Liquidity
-    function addLiquidityA(uint256 amount) public { 
+            constructor(address _token0, address _token1) 
+            {
+                tokenA = IERC20(_token0);
+                tokenB = IERC20(_token1);
+                tokenA.approve(address(this), 100);
+                tokenB.approve(address(this), 100);
+                tokenA.transferFrom(msg.sender, address(this), 100);
+                tokenB.transferFrom(msg.sender, address(this), 100);
+            }
 
-       ratio = tokenB.balanceOf(address(this))/tokenA.balanceOf(address(this));
-       
-       require(amount > 0, "Amount must be greater than 0");
-       require(tokenB.balanceOf(msg.sender) >= amount*ratio, "Insufficient balance for tokenB");
-       
-       tokenA.approve(address(this), amount);
-       tokenB.approve(address(this), amount*ratio);   
+    // 1.
+                 //  Add Liquidity for tokenA
+                function addLiquidityA(uint256 amount) public 
+                { 
+                    ratio = tokenB.balanceOf(address(this))/tokenA.balanceOf(address(this));
+                
+                    require(amount > 0, "Amount must be greater than 0");
+                    require(tokenB.balanceOf(msg.sender) >= amount*ratio, "For this amount of tokenA, you do not have enough tokenB");
+                
+                    tokenA.approve(address(this), amount);              // Burada approve edilen token kontratın mı yoksa çağıran kişiye mi ait 
+                    tokenB.approve(address(this), amount*ratio);   
+                    
+                    balanceA += amount;
+                    balanceB += amount*ratio;
 
-       tokenA.transferFrom(msg.sender, address(this), amount);   
-       tokenB.transferFrom(msg.sender,address(this), amount*ratio);
-    
-    }
+                    tokenA.transferFrom(msg.sender, address(this), amount);   
+                    tokenB.transferFrom(msg.sender,address(this), amount*ratio);
+                }
+                
+                // Add Liquidity for tokenB
+                function addLiquidityB(uint256 amount) public 
+                { 
+                    ratio = tokenA.balanceOf(address(this))/tokenB.balanceOf(address(this));
+                
+                    require(amount > 0, "Amount must be greater than 0");
+                    require(tokenA.balanceOf(msg.sender) >= amount*ratio, "For this amount of tokenB, the pool does not have enough tokenA");
+                
+                    tokenB.approve(address(this), amount);              // Burada approve edilen token kontratın mı yoksa çağıran kişiye mi ait 
+                    tokenA.approve(address(this), amount*ratio);   
 
-    // 2. Remove Liquidity
-    function removeLiquidity(uint256 amount0, uint256 amount1) public {
-       
-    }
+                    balanceB += amount;
+                    balanceA += amount*ratio;
+
+                    tokenB.transferFrom(msg.sender, address(this), amount);   
+                    tokenA.transferFrom(msg.sender,address(this), amount*ratio);
+                }
+
+    // 2.
+                // Remove Liquidity for tokenA
+                
+                function removeLiquidity(uint256 amount) public 
+                {
+                    ratio = tokenA.balanceOf(address(this))/tokenB.balanceOf(address(this));
+
+                    require(balanceA >= amount && balanceB >= amount*ratio, "You dont need have enough likidite for removing");
+                    require(amount < tokenA.balanceOf(addres(this)), "Amount must be lower than pool balance");
+                    require(amount*ratio < tokenB.balanceOf(addres(this)), "For this amount of tokenA, the pool does not have enough tokenB");
+
+                    tokenA.approve(address(this), amount);             // Burada approve edilen token kontratın mı yoksa çağıran kişiye mi ait 
+                    tokenB.approve(address(this), amount*ratio);
+
+                    balanceA -= amount;
+                    balanceB -= amount*ratio;
+
+                    tokenA.transferFrom(address(this), msg.sender, amount);
+                    tokenB.transferFrom(address(this), msg.sender, amount*ratio);
+                }
+
+                // Remove Liquidity for tokenB
+
+                function removeLiquidity(uint256 amount) public 
+                {
+                    ratio = tokenB.balanceOf(address(this))/tokenA.balanceOf(address(this));
+
+                    require(balanceB >= amount && balanceA >= amount*ratio, "You dont need have enough likidite for removing");
+                    require(amount < tokenB.balanceOf(addres(this)), "Amount must be lower than pool balance");
+                    require(amount*ratio < tokenA.balanceOf(addres(this)), "For this amount of tokenB, the pool does not have enough tokenA");
+
+                    tokenB.approve(address(this), amount);             // Burada approve edilen token kontratın mı yoksa çağıran kişiye mi ait 
+                    tokenA.approve(address(this), amount*ratio);
+
+                    balanceB -= amount;
+                    balanceA -= amount*ratio;
+
+                    tokenB.transferFrom(address(this), msg.sender, amount);
+                    tokenA.transferFrom(address(this), msg.sender, amount*ratio);
+                }
 
     // 3. Swap Tokens
-    function swap(uint256 amountIn, bool swapToken0ForToken1) public {
-       
-    }
+
+                // DÜZELTİLECEK
+
+                // function swap(uint256 amountIn, bool swapToken0ForToken1) public 
+                // {
+                //     if(swapToken0ForToken1)
+                //     {
+                //         require(amountIn < tokenA.balanceOf(address(this)), "Amount must be lower than pool balance");
+                //         tokenA.approve(address(this), amountIn);
+                //         tokenA.transferFrom(address(this), msg.sender, amountIn);
+                //         tokenB.transferFrom(msg.sender, address(this), amountIn*ratio);
+                //     }
+                //     else
+                //     {
+                //         require(amountIn < tokenB.balanceOf(address(this)), "Amount must be lower than pool balance");
+                //         tokenB.approve(address(this), amountIn);
+                //         tokenB.transferFrom(address(this), msg.sender, amountIn);
+                //         tokenA.transferFrom(msg.sender, address(this), amountIn*ratio);
+                //     }
+                // }
 
     // 4. Show Balance
-    function showBalance() public view returns (uint256 balance0, uint256 balance1) {
-       
+    function showBalance() public view returns (uint256 ,uint256 ,uint256 ,uint256)
+    {
+                 return (tokenA.balanceOf(address(this)), tokenB.balanceOf(address(this)) , balanceA, balanceB);
+
     }
 }
