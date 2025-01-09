e918259c9d3ec08074760d0cf8568dc20938cbdd
Kenar-MC <kenar.fresh@gmail.com>
2025-01-09 18:37:40 +0300

    swap function bugfix

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -137,7 +137,6 @@ contract Pool
 
                         require(amountIn > 0, "Amount must be greater than 0");
                         require(amountIn < tokenA.balanceOf(msg.sender), "You do not have enough tokenA for this swap");
-                        require(swapTokenRatio < tokenB.balanceOf(address(this)), "Amount that take must be lower than pool balance");
 
                         tokenA.approve(address(this), amountIn);
 
@@ -150,7 +149,6 @@ contract Pool
 
                         require(amountIn > 0, "Amount must be greater than 0");
                         require(amountIn < tokenB.balanceOf(msg.sender), "You do not have enough tokenB for this swap");
-                        require(swapTokenRatio < tokenA.balanceOf(address(this)), "Amount that take must be lower than pool balance");
 
                         tokenB.approve(address(this), amountIn);
 
